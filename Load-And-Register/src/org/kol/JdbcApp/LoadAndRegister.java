435a0a14ad84b1ac77f96523a4c68150522f51fb
iSemicolon <palash.samanta7497@gmail.com>
2021-12-14 18:37:43 +0530

    Load And Register the Driver [JDBC STEP-1 ]

--- Load-And-Register/src/org/kol/JdbcApp/LoadAndRegister.java
+++ Load-And-Register/src/org/kol/JdbcApp/LoadAndRegister.java
@@ -5,7 +5,7 @@ public class LoadAndRegister {
 	public static void main(String[] args) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			
+
 			System.out.println("Driver Class Loaded and Registered");
 		} 
 		catch (ClassNotFoundException e) {
