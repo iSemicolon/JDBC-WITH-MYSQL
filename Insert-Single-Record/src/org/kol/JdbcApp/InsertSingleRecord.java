10b041f1967f61f1639db5284a6c45ed094b97a5
iSemicolon <palash.samanta7497@gmail.com>
2021-12-18 10:57:35 +0530

    Insert Single Record Using Statement Interface

--- Insert-Single-Record/src/org/kol/JdbcApp/InsertSingleRecord.java
+++ Insert-Single-Record/src/org/kol/JdbcApp/InsertSingleRecord.java
@@ -10,53 +10,53 @@ public class InsertSingleRecord {
 
 	public static void main(String[] args) {
 
-				
-				Connection con=null;
-				Statement stmt=null;
-				String qry="insert into kol.student values(3,'Tiya',95.5)";
-				
+
+		Connection con=null;
+		Statement stmt=null;
+		String qry="insert into kol.student values(4,'Tiya',95.5)";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("class Loaded & Registered");
+
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=root");
+			System.out.println("Connection Established");
+
+			stmt=con.createStatement();
+			System.out.println("Statement Created Suceessfully");
+
+			stmt.executeUpdate(qry);
+			System.out.println("Statement Executed Suceessfully");
+
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		finally {
+
+			if(stmt!=null)
+			{
 				try {
-					Class.forName("com.mysql.jdbc.Driver");
-					System.out.println("class Loaded & Registered");
-					
-					con=DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=root");
-					System.out.println("Connection Established");
-					
-					stmt=con.createStatement();
-					System.out.println("Statement Created Suceessfully");
-					
-					stmt.executeUpdate(qry);
-					System.out.println("Statement Executed Suceessfully");
-						
-					
-				} catch (ClassNotFoundException | SQLException e) {
+					stmt.close();
+				} catch (SQLException e) {
 					e.printStackTrace();
 				}
-				
-				finally {
-					
-					if(stmt!=null)
-					{
-						try {
-							stmt.close();
-						} catch (SQLException e) {
-							e.printStackTrace();
-						}
-					}
-					if(con!=null)
-					{
-						
-							try {
-								con.close();
-							} catch (SQLException e) {
-								e.printStackTrace();
-							}
-						
-					}
+			}
+			if(con!=null)
+			{
+
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
 				}
 
-				System.out.println("Closed All Costly Resources");
 			}
+		}
 
+		System.out.println("Closed All Costly Resources");
 	}
+
+}
 
